7a25345bc266bc1b021db1780bb0aa2badf481ba
ndnam198 <ndnam198@gmail.com>
2020-11-20 01:23:56 +0700

    [11/20/20 01:23:56]:

--- commit.sh
+++ commit.sh
@@ -4,4 +4,4 @@ timestamp=$(date +"%D %T")
 message=$1
 git add -A;
 git commit -m "[$timestamp]: $message"
-git push https://github.com/ndnam198/-DACN-HM10_SHT3x_STM32.git master
+git push https://github.com/ndnam198/_DACN_HM10_SHT3x_STM32.git master
